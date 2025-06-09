040f06f7207245dfc421cc5d948bd8afb1c031b7
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2025-06-09 13:09:59 -0700

    chore: format

--- src/speaches/types/realtime.py
+++ src/speaches/types/realtime.py
@@ -619,11 +619,13 @@ ServerEvent = Annotated[
 
 server_event_type_adapter = TypeAdapter[ServerEvent](ServerEvent)
 
+
 # Message fragmentation types for WebRTC
 class FullMessageEvent(BaseModel):
     id: str
     type: Literal["full_message"] = "full_message"
     data: str
+
 
 class PartialMessageEvent(BaseModel):
     id: str
@@ -631,6 +633,7 @@ class PartialMessageEvent(BaseModel):
     data: str
     fragment_index: int
     total_fragments: int
+
 
 type MessageFragment = FullMessageEvent | PartialMessageEvent
 
